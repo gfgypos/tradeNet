fbc0bea741c48ee0e507d6094ff20892f2dc2909
johnyboy63 <jd_stewart_6312@hotmail.com>
2015-11-16 20:21:43 -0600

    Adding login form python script

--- classes.py
+++ classes.py
@@ -9,6 +9,7 @@ class User:
 	password = ""
 	name = ""
 	is_active = 0
+	error_code = 0
 	def __init__(self):
 		pass
 
@@ -22,17 +23,17 @@ class User:
 					cur.execute("SELECT is_active FROM user WHERE username=?", [username])
 					active = cur.fetchone()[0]
 					if active == 0:
-						print("\nAccount suspended! Contact admin...")
+						self.error_code = 1
 						return False
 					else:
 						return True
 				
 			else:
-				print "ERROR: invalid passowrd"
+				self.error_code = 2
 				return False
 		else:
 			print cur.fetchone()
-			print "ERROR: username does not exist"
+			self.error_code = 3
 			return False
 
 class Admin(User):
@@ -88,11 +89,6 @@ class Account:
 
 	def checkBalance(self):
 		return self.account_bal
-
-class Brokerage(Account):
-	def __init__self(self, accountNum):
-		self.accountNum = accountNum
-		self.accountType = "brokerage"
 
 class Savings(Account):
 	def __init__(self, accountNum):
