bbef5fece908977a62277c43cce53656703832d3
Aaron Van Dan <arv34@msstate.edu>
2015-12-04 12:45:22 -0600

    Added comments to execute_buy to help track down error

--- tradeNet_web/execute_buy.php
+++ tradeNet_web/execute_buy.php
@@ -18,7 +18,7 @@
  		 if($user_bal >= $total_cost)
  		 {
  		 	$current_date = date('Y-m-d H:i:s');
- 		 	
+ 		 	// this always returns true, even with no rows in db	
  		 	if($stmt = $dbHandle->prepare("SELECT shares FROM brokerage_portfolio where uid=:uid AND stock=:stock "))
  		 	{
  		 		$stmt->bindParam(':uid', $uid, PDO::PARAM_INT);
@@ -27,6 +27,7 @@
  		 		$result = $stmt->fetch(PDO::FETCH_ASSOC);	
 
  		 		$new_num_shares = $result['shares'] + $num_share;
+				/* the way we have it set up, we store the individual stock purchase price, that way we can easily check to see if the newer price is > or < current market price for profit loss as well as display that info to users. Looks like you're storing TOTAL cost */
  		 		$stmt = $dbHandle->prepare("UPDATE brokerage_portfolio SET shares=:shares, purchase_price=purchase_price+:cost WHERE uid=:uid AND stock=:stock");
  		 		$stmt->bindParam(':user', $uid, PDO::PARAM_INT);
  		 		$stmt->bindParam(':symbol', $share_name, PDO::PARAM_STR);
@@ -34,6 +35,7 @@
  		 		$stmt->bindParam(':cost', $total_cost, PDO::PARAM_STR);
  		 		$stmt->execute();
  		 	}
+			/* I couldn't ever get this to return true or execute so some logic is funky around here */
  		 	else
  		 	{
  		 		$stmt = $dbHandle->prepare("INSERT INTO brokerage_portfolio VALUES (':user', ':symbol', ':shares', ':cost')");
@@ -64,6 +66,7 @@
  		 	$stmt->bindParam(':cost', $total_cost, PDO::PARAM_STR);
  		 	$stmt->bindParam(':acct', $user_acct, PDO::PARAM_INT);
  		 	$stmt->execute();
+			/* It will always say transaction successful even though these statements are failing */
 
  		 	echo "<h1 style='text-align: center'>Transaction Successful</h1>";
 			echo "<a style='text-align: center' href='buystock.php'>Return to Searching for Stocks</a>";
